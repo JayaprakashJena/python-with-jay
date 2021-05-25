2168ee9ce9a8f620f148da8cb0f14c7bdbf56b10
= <jayaprakashjena98@gmail.com>
2021-05-25 17:26:20 +0530

    commit 1

---
+++ if_elif_else.py.py
@@ -0,0 +1,18 @@
+# a=int(input('enter 1st num'))
+# b=int(input('enter 2nd num'))
+# c=int(input('enter 3rd num'))
+# if (a>b and a>c):
+#     print('a is greatest')
+
+# elif (b>a and b>c):
+#     print('b is greatest')
+
+# elif(c>a and c>b ):
+#     print('c is greatest')
+
+num=int(input('enter any number'))
+if (num>0 and num %2==0):
+    print('even')
+
+else:
+    print('odd')
