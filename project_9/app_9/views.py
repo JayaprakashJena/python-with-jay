948e2c3b02a005ad0a943b5c462d750e82abf234
= <jayaprakashjena98@gmail.com>
2021-06-05 03:06:53 +0530

    project_9

---
+++ project_9/app_9/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def showIndex(request):
+    employee={'data': 
+           [{'idno':101,'class':'one','name':'jay','cno':358284667},
+           {'idno':102,'class':'two','name':'naveen','cno':358284667},
+           {'idno':103,'class':'three','name':'Raj','cno':358284667},
+           {'idno':103,'class':'three','name':'Raj','cno':358284667},
+           {'idno':104,'class':'four','name':'Ravi','cno':358284667},
+           ]      
+           }
+    return render(request,'emp_file.html',employee)
