57c92ce08d73a0f147c1708eaeb32c3329c953bc
= <jayaprakashjena98@gmail.com>
2021-06-12 21:46:18 +0530

    project15

--- Project_14/app_14/views.py
+++ Project_14/app_14/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 def showIndex(request):
     return render(request,'index.html')
 def loginCheckout(request):
-    Username=request.POST.get("t1")
+    if request== 'POST':
+    username=request.POST.get("t1")
     password=request.POST.get("t2")
     if Username == 'jay' and password =='jay123' :
         return render(request,"welcome.html")
